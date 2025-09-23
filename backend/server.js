a23428f29ffeed14eb1a7f26290d4150350f0e6c
Rajatkumar Moolya <rajatkumarmoolya@gmail.com>
2025-09-23 12:40:02 +0530

    Prepare backend for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URL
+, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
